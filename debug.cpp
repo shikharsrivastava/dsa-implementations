b9fcf8b65095521c995979d8a25a83cdbec16339
Shubham_Bohra <shubhambohra0009@gmail.com>
2021-10-26 00:31:04 +0530

    added debug.cpp

---
+++ debug.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x=5;
+    for (int i = 0; i < 5; i++)
+    {
+        cout<<i+1<<endl;
+        x--;
+    }
+    
+
+
+    return 0;
+}
